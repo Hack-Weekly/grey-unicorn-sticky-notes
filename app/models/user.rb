c42a6e7e8ac1e4452f68acefec79acb9af9a8a07
stuart wilson <stuartwilson94@gmail.com>
2023-07-19 11:27:38 -0500

    added active storage and created users

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  has_many :comments, dependent: :destroy
+  has_many :sticky_notes, as: :author, dependent: :destroy
+
+  validates :name, presence: true
+
+  def first_name
+    name.split.first
+  end
+
+  def last_name
+    name.split.last
+  end
+end
