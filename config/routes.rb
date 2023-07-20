199a26bccb24089c2b87d6f20d4e242dd4af806c
Brandon Campbell <bmc1022@gmail.com>
2023-07-20 13:46:14 +1000

    organize routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :sticky_notes
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
 
   root to: "sticky_notes#index"
+
+  resources :users do
+    resources :sticky_notes
+  end
 end
