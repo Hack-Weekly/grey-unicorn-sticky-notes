2d95da07bbf5245ae8955ed706e3e269e9ff3d7f
Brandon Campbell <bmc1022@gmail.com>
2023-07-20 13:47:15 +1000

    devise user controller changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,12 +38,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-   protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(
       :sign_up,
@@ -57,10 +54,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
       keys: %i[name profile]
     )
   end
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
