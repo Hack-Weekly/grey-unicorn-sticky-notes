9e624274238a270f09fecef8aeced28c54df5ba1
Brandon Campbell <bmc1022@gmail.com>
2023-07-21 23:42:14 +1000

    rolify setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ApplicationRecord
+  rolify
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :assign_default_role
 
   has_many :comments, dependent: :destroy
   has_many :sticky_notes, as: :author, dependent: :destroy
-  has_many :roles, dependent: :destroy
 
   validates :name, presence: true
+  validate :must_have_a_role, on: :update
 
   def first_name
     name.split.first
@@ -18,5 +20,15 @@ class User < ApplicationRecord
 
   def last_name
     name.split.last
+  end
+
+  private
+
+  def assign_default_role
+    add_role(:registered) if roles.blank?
+  end
+
+  def must_have_a_role
+    errors.add(:roles, "must have at least one role") unless roles.any?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_022319) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_105025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_022319) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  end
+
   create_table "sticky_notes", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -70,6 +80,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_022319) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
