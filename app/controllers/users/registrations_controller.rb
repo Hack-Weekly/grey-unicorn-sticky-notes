313c794df54e17b376b82e42815ba50dc35064fd
Brandon Campbell <bmc1022@gmail.com>
2023-07-22 17:54:03 +1000

    skip authorization callback in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  skip_after_action :verify_authorized, only: [:create, :new, :destroy]
+
   # GET /resource/sign_up
   # def new
   #   super
