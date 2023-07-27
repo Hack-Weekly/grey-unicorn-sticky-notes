8c770d666089099334f11b1b8ff7ce8f82e29c14
Brandon Campbell <bmc1022@gmail.com>
2023-07-27 18:18:46 +1000

    refactor stimulus note_controller and add clause to reject blank notes from submitting

--- app/javascript/controllers/note_controller.js
+++ app/javascript/controllers/note_controller.js
@@ -2,17 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["textarea"];
+
   connect() {
     console.log("Note controller connected.");
   }
-  enter(e) {
-    if (e.which == 13) {
-      this.textareaTarget.setAttribute("data-action", "");
-      this.element.requestSubmit();
+
+  rejectBlankNote() {
+    return this.textareaTarget.value.trim() != ""
+  }
+
+  submitNote(e) {
+    if (!this.rejectBlankNote()) {
+      return;
     }
+
     e.preventDefault();
+    this.textareaTarget.setAttribute("data-action", "");
     this.element.requestSubmit();
-    e.stopImmediatePropagation();
-    return false;
   }
 }
