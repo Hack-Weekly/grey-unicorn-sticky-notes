b6ba8a47fb5e73a44d54d0124174d2596828ee03
stuart wilson <stuartwilson94@gmail.com>
2023-07-28 17:48:12 -0500

    added mrujs for drag controller function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,13 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-import "@rails/actiontext"
-import "trix"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import * as bootstrap from "bootstrap";
+import "@rails/actiontext";
+import "trix";
 
+import mrujs from "mrujs";
+import * as Turbo from "@hotwired/turbo";
+
+window.Turbo = Turbo;
+
+mrujs.start();
