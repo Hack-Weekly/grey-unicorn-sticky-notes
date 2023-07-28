883d9726b11d301775d06cf68f2a0ff7f61484ea
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-28 16:46:05 -0700

    Add select sticky note color feature

--- app/javascript/controllers/note_controller.js
+++ app/javascript/controllers/note_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["textarea"];
+  static targets = ["textarea", "select", "form"];
 
   connect() {
     console.log("Note controller connected.");
   }
 
   rejectBlankNote() {
-    return this.textareaTarget.value.trim() != ""
+    return this.textareaTarget.value.trim() != "";
   }
 
   submitNote(e) {
@@ -19,5 +19,22 @@ export default class extends Controller {
     e.preventDefault();
     this.textareaTarget.setAttribute("data-action", "");
     this.element.requestSubmit();
+  }
+
+  changeSelectColor(e) {
+    const selectedColor =
+      this.selectTarget.options[this.selectTarget.selectedIndex].value;
+    this.selectTarget.setAttribute(
+      "style",
+      `background-color: ${selectedColor};`
+    );
+    this.formTarget.setAttribute(
+      "style",
+      `background-color: ${selectedColor};`
+    );
+    this.textareaTarget.setAttribute(
+      "style",
+      `background-color: ${selectedColor};`
+    );
   }
 }
