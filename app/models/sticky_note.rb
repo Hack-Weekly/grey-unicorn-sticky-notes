297767a50236a55ef4a72da7d8dbc80d3e91cad0
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-08-01 14:04:17 -0700

    Add turbo stream for dynamic completion of sticky notes

--- app/models/sticky_note.rb
+++ app/models/sticky_note.rb
@@ -1,6 +1,8 @@
 class StickyNote < ApplicationRecord
   belongs_to :owner, polymorphic: true
   belongs_to :whiteboard
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
 
   def self.by_position
     order("position ASC")
