e0a2f1e6b6a8bba2013c22df7132a9827e1f00fa
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-08-02 10:10:31 -0700

    Clean up routes and view helpers

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
   root to: "dashboard#index"
 
   resources :sticky_notes, except: [:index]
+  resources :whiteboards do
+    get "/completed", to: "whiteboards#completed"
+  end
 
   resources :workspaces do
     resources :whiteboards do
       resources :sticky_notes, except: [:index]
-      get "/completed", to: "whiteboards#completed"
     end
   end
 end
